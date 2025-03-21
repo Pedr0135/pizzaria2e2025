51a17d6c6bdc9823792e07394e0fa1938e36ba30
Pedr0135 <rm95165@estudante.fieb.edu.br>
2025-03-21 09:40:12 -0300

    projeto pizzaria

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import{useState} from "react"
+
+function App(){
+
+  const[nome, setNome] = useState('Ronaldo')
+
+return (
+
+  <div>
+
+    
+    <h3>Pizzaia 2E</h3>
+
+    <input 
+    className="Nome"
+    onChange={(e)=>{setNome(e.target.value)}}
+    type="text" />
+
+    <button
+    className="botao"
+    onClick={()=>{alert(nome)}}
+    >
+
+  CLIQUE AQUI
+
+    </button>
+
+
+  </div>
+
+)
+
+}
+
+
+export default App
